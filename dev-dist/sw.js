9e65d249b8170f364dcabfd75f932c3bff3d0e5e
binhxd04 <nqhidanq7@gmail.com>
2025-09-20 17:32:44 +0700

    upload screenshots

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b8c89818'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.lr68vn3ilhg"
+    "revision": "0.anu1takecko"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
